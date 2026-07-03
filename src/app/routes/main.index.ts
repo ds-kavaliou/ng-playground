cd1399dd4e3ae5b8edcda015b9c7fa5e0c92e1f3
dzianis kavaliou <ds.kavaliou@gmail.com>
2026-07-03 10:51:37 +0200

    chore: eslint/settings/tailwind

--- src/app/routes/main.index.ts
+++ src/app/routes/main.index.ts
@@ -1,14 +1,31 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { afterEveryRender, afterNextRender, Component, effect, signal } from '@angular/core';
+import { HlmBadge } from '@app/ui/badge';
 import { HlmButton } from '@app/ui/button';
 
 @Component({
   selector: 'app-index-page',
-  imports: [HlmButton],
-  changeDetection: ChangeDetectionStrategy.Eager,
+  imports: [HlmButton, HlmBadge],
   template: `
-    <div>
-      <button hlmBtn>Click me</button>
-    </div>
+    <section class="container">
+      <button hlmBtn (click)="counter.update((x) => x + 1)">Click me</button>
+      <hlm-badge>{{ counter() }}</hlm-badge>
+    </section>
   `,
 })
-export class MainIndexPage {}
+export class MainIndexPage {
+  readonly counter = signal(0);
+
+  constructor() {
+    afterEveryRender(() => {
+      console.log('afterEveryRender rendered');
+    });
+
+    afterNextRender(() => {
+      console.log('afterNextRender rendered for the first time', this.counter());
+    });
+
+    effect(() => {
+      console.log('effect rendered');
+    });
+  }
+}
--- src/app/routes/main.ts
+++ src/app/routes/main.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,21 +6,29 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   imports: [RouterOutlet, RouterLink],
   template: `
     <header class="shadow">
-      <div class="container h-20 flex items-center justify-between">
-        <h1>playground</h1>
+      <div class="container h-16 flex items-center justify-between">
+        <h1 class="font-medium tracking-wide uppercase">
+          <a routerLink="/">playground.</a>
+        </h1>
         <nav class="flex items-center gap-4">
           <a routerLink="/">home</a>
         </nav>
       </div>
     </header>
 
-    <main class="grow py-4">
+    <main class="grow py-2">
       <router-outlet />
     </main>
 
-    <footer>footer</footer>
+    <footer class="border-t py-4">
+      <div class="container">
+        <p class="text-sm text-center">
+          &copy; <span class="font-semibold uppercase tracking-wide">playground.</span> All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
   `,
-  changeDetection: ChangeDetectionStrategy.Eager,
   host: {
     class: 'min-h-screen flex flex-col',
   },
