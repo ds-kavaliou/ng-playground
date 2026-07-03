483a604beed394dca1ca66f31f8dbb45aa5dfebd
dzianis kavaliou <ds.kavaliou@gmail.com>
2026-07-03 09:19:03 +0200

    feat: init spartan.ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('@app/routes/main').then((m) => m.MainLayout),
+    loadComponent: () => import('./routes/main').then((m) => m.MainLayout),
     children: [
-      { path: '', loadComponent: () => import('@app/routes/main.index').then((m) => m.MainIndexPage) },
+      { path: '', loadComponent: () => import('./routes/main.index').then((m) => m.MainIndexPage) },
     ],
   },
 ];
--- src/app/routes/main.index.ts
+++ src/app/routes/main.index.ts
@@ -1,9 +1,14 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { HlmButton } from '@app/ui/button';
 
 @Component({
   selector: 'app-index-page',
-  imports: [],
+  imports: [HlmButton],
   changeDetection: ChangeDetectionStrategy.Eager,
-  template: `<div>index page works</div>`,
+  template: `
+    <div>
+      <button hlmBtn>Click me</button>
+    </div>
+  `,
 })
 export class MainIndexPage {}
