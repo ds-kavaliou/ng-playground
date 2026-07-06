b4f4f236e995d85ef208887c15148085d8d471c5
dzianis kavaliou <ds.kavaliou@gmail.com>
2026-07-06 08:47:25 +0200

    fix: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,14 @@ export const routes: Routes = [
     loadComponent: () => import('./routes/main').then((m) => m.MainLayout),
     children: [
       { path: '', loadComponent: () => import('./routes/main.index').then((m) => m.MainIndexPage) },
+      {
+        path: 'flights',
+        loadComponent: () => import('./routes/main.flights').then((m) => m.MainFlightsPage),
+      },
+      {
+        path: 'signals',
+        loadComponent: () => import('./routes/main.signals').then((m) => m.MainSignalsPage),
+      },
     ],
-  },
-  {
-    path: 'flights',
-    loadComponent: () => import('./routes/main.flights').then((m) => m.MainFlightsPage),
-  },
-  {
-    path: 'signals',
-    loadComponent: () => import('./routes/main.signals').then((m) => m.MainSignalsPage),
   },
 ];
--- src/app/routes/main.flights.ts
+++ src/app/routes/main.flights.ts
@@ -1,17 +1,31 @@
 import { Component, effect, resource, signal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { form, FormField, FormRoot } from '@angular/forms/signals';
+import { HlmButtonImports } from '@app/ui/button';
 
-import { HlmBadge } from '@app/ui/badge';
-import { HlmButton } from '@app/ui/button';
+import { HlmFieldImports } from '@app/ui/field';
+import { HlmInput } from '@app/ui/input';
 
 const API_URL = 'https://demo.angulararchitects.io/api/flight';
 
 @Component({
   selector: 'app-flights-page',
-  imports: [FormField, HlmButton, HlmBadge],
+  imports: [FormField, FormRoot, HlmFieldImports, HlmButtonImports, HlmInput],
   template: `
     <section class="container">
-      <form>1</form>
+      <form [formRoot]="f" class="flex items-end gap-4">
+        <div hlmField>
+          <label hlmFieldLabel for="from">From</label>
+          <input hlmInput id="from" [formField]="f.from" />
+        </div>
+        <div hlmField>
+          <label hlmFieldLabel for="to">To</label>
+          <input hlmInput id="to" [formField]="f.to" />
+        </div>
+
+        <div hlmField>
+          <button hlmBtn type="submit" variant="outline">Search</button>
+        </div>
+      </form>
     </section>
   `,
 })
